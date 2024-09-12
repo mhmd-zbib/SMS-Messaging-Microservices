ab08557058d4824cc42cfa7222c6fa6e27319683
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-09-12 09:42:51 +0300

    init

---
+++ Provider1/src/test/java/dev/zbib/provider1/Provider1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zbib.provider1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provider1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Provider2/src/test/java/dev/zbib/provider2/Provider2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zbib.provider2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provider2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
