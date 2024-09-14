2808edddc8a76cbd0be591f6798e42706018ba09
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-09-14 13:45:35 +0300

    fix: rename provider 2 to MtcProvider

--- Provider2/src/main/java/dev/zbib/provider2/MtcProviderApplication.java
+++ Provider2/src/main/java/dev/zbib/provider2/MtcProviderApplication.java
@@ -8,7 +8,7 @@ public class Provider2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Provider2Application.class, args);
-        System.out.println("Welcome to Alfa");
+        System.out.println("Welcome to Mtc");
 
     }
 
