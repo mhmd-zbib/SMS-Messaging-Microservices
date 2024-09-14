4fed5f2514d60d23808468813a83319c056dae1e
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-09-14 16:14:22 +0300

    fix: modify req params for mtc provider

--- MtcProvider/src/main/java/dev/zbib/provider2/SmsController.java
+++ MtcProvider/src/main/java/dev/zbib/provider2/SmsController.java
@@ -2,26 +2,25 @@ package dev.zbib.provider2;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class SmsController {
 
 
-
     @PostMapping
-    public ResponseEntity<String> sendCode(
-//            @RequestBody MessageReq messageReq
-    ) {
-//        System.out.println(messageReq.getPhoneNumber() +
-//                " says: " + messageReq.getMessage() +
-//                " talking in: " + messageReq.getLanguage());
-//
-//        return ResponseEntity.ok(messageReq.getPhoneNumber() +
-//                " says: " + messageReq.getMessage() +
-//                " talking in: " + messageReq.getLanguage());
-        return ResponseEntity.ok("working");
+    public ResponseEntity<String> sendCode(@RequestBody MessageReq messageReq) {
+        System.out.println(messageReq.getPhoneNumber() +
+                " says: " + messageReq.getMessage() +
+                " talking in: " + messageReq.getLanguage());
+
+        return ResponseEntity.ok(messageReq.getPhoneNumber() +
+                " says: " + messageReq.getMessage() +
+                " talking in: " + messageReq.getLanguage());
     }
 
 }
