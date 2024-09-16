280159fe8ecfd0b7ae8108e0348e4aabf86073bb
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-09-16 21:36:37 +0300

    fix: enhance error handling

--- Server/src/main/java/dev/zbib/server/exception/Exceptions/ResourceNotFoundException.java
+++ Server/src/main/java/dev/zbib/server/exception/Exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package dev.zbib.server.exception;
+package dev.zbib.server.exception.Exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
