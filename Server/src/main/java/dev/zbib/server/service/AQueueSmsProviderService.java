701661b7da886d2dda55fd2f64280d46da69e7b4
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-09-26 23:40:53 +0300

    docs: Queue sms provider service docs

--- Server/src/main/java/dev/zbib/server/service/AQueueSmsProviderService.java
+++ Server/src/main/java/dev/zbib/server/service/AQueueSmsProviderService.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
 
+/**
+ * <h2>Abstract class for SMS queue providers</h2>
+ * <p>This class implements the ISmsProviderService taking all the methods that all the providers do,
+ * and here it uses them depending on its context. Using bean {@link RabbitTemplate} bean in config.
+ */
+
+
 public abstract class AQueueSmsProviderService implements ISmsProviderService {
     private final String queueName;
 
@@ -16,6 +23,12 @@ public abstract class AQueueSmsProviderService implements ISmsProviderService {
     protected AQueueSmsProviderService(String queueName) {
         this.queueName = queueName;
     }
+
+    /**
+     * <h3>SMS message sender</h3>
+     * <p>This service handles sending sms to the queue provider. It passes the request params to the
+     * post api from the smsProviderRequest</p>
+     */
 
     @Override
     public Mono<String> sendSms(SmsProviderRequest smsProviderRequest) {
