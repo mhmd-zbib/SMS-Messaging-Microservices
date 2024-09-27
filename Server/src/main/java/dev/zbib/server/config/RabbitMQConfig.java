d61492b99f2a22b574560587507bb1df811683ac
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-09-27 15:13:59 +0300

    feat: add connection factory and template bean in rabbitMQ

--- Server/src/main/java/dev/zbib/server/config/RabbitMQConfig.java
+++ Server/src/main/java/dev/zbib/server/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package dev.zbib.server.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,13 +11,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${listener.queue}")
+    private String listenerQueue;
+
+    @Value("${cron.queue}")
+    private String cronQueue;
+
     @Bean
-    public Queue smsListenerQueue() {
-        return new Queue("sms-listener", true);
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
     }
 
     @Bean
-    public Queue smsCronQueue() {
-        return new Queue("sms-cron", true);
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        System.out.println("i was called here");
+        return new RabbitTemplate(connectionFactory);
     }
+
+
 }
